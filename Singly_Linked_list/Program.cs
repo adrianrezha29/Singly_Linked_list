9680f100ea0438a089f2e50a5d3523f8df908056
adrianrezha29 <adrianrezha29@gmail.com>
2022-11-11 20:35:43 +0700

    add method console writeline for menu

--- Singly_Linked_list/Program.cs
+++ Singly_Linked_list/Program.cs
@@ -98,7 +98,17 @@ namespace Singly_Linked_list
             {
                 try
                 {
-                }
+                    Console.WriteLine("\nMENU");
+                    Console.WriteLine("1. Add a record to the list ");
+                    Console.WriteLine("2. Delete a record from the list");
+                    Console.WriteLine("3. View all the records in the list");
+                    Console.WriteLine("4. Search for a record in the list");
+                    Console.WriteLine("5. EXIT");
+                    Console.Write("\nEnter your choice (1-5) : ");
+                    char ch = Convert.ToChar(Console.ReadLine());
+                    switch (ch)
+                    {
+                    
 
 
             }
