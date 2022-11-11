27bb42eae44020e98f16efbc9c2030d096e9830d
adrianrezha29 <adrianrezha29@gmail.com>
2022-11-11 17:21:02 +0700

    add method nim,nm

--- Singly_Linked_list/Program.cs
+++ Singly_Linked_list/Program.cs
@@ -4,11 +4,32 @@ namespace Singly_Linked_list
 {
     //each node consist of the information part and link to the next mode
 
-    class node
+    class Node
     {
         public int rollNumber;
         public string name;
-        public node next;
+        public Node next;
     }
-    class list
+    class List
+    {
+        Node Start;
+
+        public List()
+        {
+            Start = null;
+        }
+        public void addNote() //add node in the list
+        {
+            int nim;
+            string nm;
+            Console.WriteLine("\nEnter the roll number of the student : ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nEnter the name of the student : ");
+            nm = Console.ReadLine();
+            Node newnode = new Node();
+            newnode.rollNumber = nim;
+            newnode.name = nm;
+            //if the node to be inserted is the first node
+        }
+    }
 }
